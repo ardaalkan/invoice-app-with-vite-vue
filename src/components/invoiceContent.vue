63c63ce3ec7ff93efc219176cfd9581db9c7b998
ardaalkan <ardaaa.alk@gmail.com>
2023-02-22 02:10:36 +0300

    app-initialized

---
+++ src/components/invoiceContent.vue
@@ -0,0 +1,33 @@
+<template>
+  <section
+    class="bg-gray-900 w-1/3 mx-auto mt-10 p-2 px-5 rounded-md shadow-2xl"
+  >
+    <contact-section :contact="state.contact"/>
+    <div class="mt-5">
+      <heading title="Items" />
+      <invoice-items :items="state.items"/>
+    </div>
+    <!-- Summary -->
+    <invoice-summary />
+    <hr class="bg-gradient-to-r h-[1px] border-none from-gray-700 mt-5" />
+    <div class="actionbar text-right my-5">
+      <button class="purple-button">Submit</button>
+    </div>
+  </section>
+</template>
+<script setup>
+import invoiceItems from "../components/invoiceItems.vue";
+import invoiceSummary from "../components/invoiceSummary.vue";
+import contactSection from "../components/contactSection.vue";
+import { reactive } from "vue";
+const state = reactive({
+  contactInfo : {
+    contact_name : null,
+    email : null,
+    city : null,
+    country : null,
+    zipcode : null,
+  },
+  items: [],
+})
+</script>
---
+++ src/components/invoiceSummary.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="summary mt-5 flex justify-end items-end">
+    <div class="text-right mr-2">
+      <div class="text-gray-500">Subtotal :</div>
+      <div class="text-gray-400">Tax :</div>
+      <div class="text-gray-300">Total Amount :</div>
+    </div>
+    <div class="text-right">
+      <div class="text-gray-500">100,00</div>
+      <div class="text-gray-400">18,00</div>
+      <div class="text-gray-300">118,00</div>
+    </div>
+  </div>
+</template>
